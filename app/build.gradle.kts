ad2fb96ea781c9cb4e5f84ffc9d9c5590db62397
Athul <athulikumar0507@gmail.com>
2024-01-25 00:23:01 +0530

    partial update of home screen(added slide button)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("com.github.yuyakaido:CardStackView:v2.3.4")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.ncorti:slidetoact:0.11.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
 
 
